414313cf12ed41066153ff9b86ee7e1f4dc64639
AmitNimrodi <animrody@gmail.com>
2020-04-09 12:40:11 +0300

    first commit

---
+++ svm_handler.py
@@ -0,0 +1,11 @@
+from sklearn import svm
+from parse_data import parse_data
+from calc_error_pct import *
+import tkMessageBox
+
+
+class SVMHandler:
+    """ In this class you will implement the classifier and it's methods. """
+    def __init__(self):
+
+
